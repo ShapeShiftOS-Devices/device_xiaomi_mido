8ffac4952baab448c00f70561b40b876b3cc5445
ArixElo <patroxgamer@gmail.com>
2021-06-05 07:44:33 +0000

    mido: vendorsetup: Remove Proton Clang toolchain
    It's there in SSOS for a some time
    
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,8 +5,5 @@ rm -rf hardware/qcom-caf/msm8996/audio && git clone https://github.com/shashank1
 rm -rf hardware/qcom-caf/msm8996/display && git clone https://github.com/shashank1436/android_hardware_qcom_display.git hardware/qcom-caf/msm8996/display
 rm -rf hardware/qcom-caf/msm8996/media && git clone https://github.com/shashank1436/android_hardware_qcom_media.git hardware/qcom-caf/msm8996/media
 
-### Cloning Proton CLang toolchain ###
-git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
-
 ### Removing and cloning working CAF Telephony ###
 rm -rf vendor/codeaurora/telephony && git clone https://github.com/shashank1436/platform_vendor_codeaurora_telephony.git vendor/codeaurora/telephony
