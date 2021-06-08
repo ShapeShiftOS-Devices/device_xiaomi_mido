4fcc855d2d10ad7693f1fb6d4ed297103a1bdd87
ArixElo <patroxgamer@gmail.com>
2021-06-08 19:45:44 +0200

    mido: vendorsetup: Add PixelExperience Telephony
    Inspired by this commit: https://github.com/ShapeShiftOS-Devices/device_oneplus_msm8998-common/commit/7e513e13f8a159b5cd0647e0b156b981e9de60ef
    
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,5 +5,7 @@ rm -rf hardware/qcom-caf/msm8996/audio && git clone https://github.com/shashank1
 rm -rf hardware/qcom-caf/msm8996/display && git clone https://github.com/shashank1436/android_hardware_qcom_display.git hardware/qcom-caf/msm8996/display
 rm -rf hardware/qcom-caf/msm8996/media && git clone https://github.com/shashank1436/android_hardware_qcom_media.git hardware/qcom-caf/msm8996/media
 
-### Removing and cloning working CAF Telephony ###
-rm -rf vendor/codeaurora/telephony && git clone https://github.com/shashank1436/platform_vendor_codeaurora_telephony.git vendor/codeaurora/telephony
+### Removing and cloning Telephony stuff ###
+rm -rf vendor/codeaurora/telephony && git clone https://github.com/PixelExperience/vendor_codeaurora_telephony.git vendor/codeaurora/telephony
+rm -rf frameworks/opt/telephony/ && git clone https://github.com/PixelExperience/frameworks_opt_telephony.git frameworks/opt/telephony/
+rm -rf packages/services/Telephony/ && git clone https://github.com/PixelExperience/packages_services_Telephony.git packages/services/Telephony/
